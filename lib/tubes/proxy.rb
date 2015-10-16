9b26c149fb48898c0f177a1d4aa0599c04728723
Chris Constantine <cconstan@gmail.com>
2015-10-15 23:12:13 -0700

    minor refinement

--- lib/tubes/proxy.rb
+++ lib/tubes/proxy.rb
@@ -1,8 +1,6 @@
-require 'celluloid'
+require 'celluloid/current'
 require 'celluloid/io'
-#require 'celluloid/autostart'
 require 'celluloid/debug'
-
 
 module Tubes
   class Proxy
@@ -10,7 +8,7 @@ module Tubes
     finalizer :shutdown
 
     def initialize(host, port)
-      puts "*** Starting proxy server on #{host}:#{port}"
+      
 
       @server = Celluloid::IO::TCPServer.new(host, port)
     end
@@ -21,27 +19,43 @@ module Tubes
 
     def run
       loop {
-        puts "Waiting for accept..."
+        #puts "Waiting for accept..."
+        puts "*** Starting proxy server"
         socket = @server.accept
-        puts "Got a socket"
-        async.handle_connection socket
+        proxy_socket = Celluloid::IO::TCPSocket.new 'localhost', 80
+        
+        puts "Got a socket: #{socket.object_id}"
+        puts "connected to: #{proxy_socket.object_id}"
+
+        handle_connection socket, proxy_socket
       }
     end
 
-    def handle_connection(socket)
-      proxy_socket = TCPSocket.new 'localhost', 80
-      async.proxy_socket(proxy_socket,socket)
-      proxy_socket(socket, proxy_socket)
+    def handle_connection(socket, proxy_socket)
+      async.proxy_socket(proxy_socket,socket, 1)
+      async.proxy_socket(socket, proxy_socket, 2)
     end
 
-    def proxy_socket(socket_from, socket_to)
+    def close_socket(s)
+      s.close
+    rescue Exception => e
+      puts "#{s.object_id} raise error when closing: #{e.inspect}"
+    end
 
-      loop do
-        socket_from.write socket_to.readpartial(4096) 
+    def proxy_socket(socket_from, socket_to, n)
+      puts "#{n}: Proxying #{socket_from.object_id} to #{socket_to.object_id}"
+
+      begin
+        buf = ''
+        loop do
+          socket_to.write socket_from.readpartial(4096, buf) 
+        end
+      rescue Exception => e
+        close_socket socket_to
+        close_socket socket_from
       end
-    rescue EOFError
-      socket_from.close
-      socket_to.close
+    ensure
+      puts "#{n}: Finished #{socket_from.object_id} to #{socket_to.object_id}"
     end
   end
 end
